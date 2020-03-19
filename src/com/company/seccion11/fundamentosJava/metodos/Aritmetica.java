1c4bacea0ac1d4af1e22ce1ee4364ad2d788ce02
unzalulide <lidiagonzalolopez@gmail.com>
2020-03-19 12:58:51 +0100

    rama partida a partir de develop metodos

---
+++ src/com/company/seccion11/fundamentosJava/metodos/Aritmetica.java
@@ -0,0 +1,10 @@
+package com.company.seccion11.fundamentosJava.metodos;
+
+public class Aritmetica {
+    public int sumar(int a, int b){
+        int resultado=a+b;
+        return resultado;
+
+
+    }
+}
---
+++ src/com/company/seccion11/fundamentosJava/metodos/Main.java
@@ -0,0 +1,11 @@
+package com.company.seccion11.fundamentosJava.metodos;
+import com.company.seccion11.fundamentosJava.metodos.Aritmetica;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Aritmetica a1=new Aritmetica();
+        int resultado=a1.sumar(5,7);
+        System.out.println(resultado);
+    }
+}
